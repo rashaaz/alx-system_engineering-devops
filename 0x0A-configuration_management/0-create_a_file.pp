7e4004f80131bd26513181102dcbe458dc33b46e
rashaaz <rashaabdelaziz111@gmail.com>
2024-02-22 20:36:09 -0800

    add 0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Puppet manifest to create a file in /tmp with specific permissions
+
+file { '/tmp/school':
+  content =>'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
