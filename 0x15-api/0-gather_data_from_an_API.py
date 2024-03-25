543cdf0153eea3d0e98cf49798e8a6c5f429087f
rashaaz <rashaabdelaziz111@gmail.com>
2024-03-24 22:43:44 -0700

    add 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     us = requests.get(url + "users/{}".format(sys.argv[1])).json()
     to = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    co = [t.get("title") for t in to if t.get("completed") is True]
+    co = [tt.get("title") for tt in to if tt.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         us.get("name"), len(co), len(to)))
-    [print("\t {}".format(c)) for c in co]
+    [print("\t {}".format(cc)) for cc in co]
