ba6a0175aa933aab28762ba0252d4c12fb1148dd
rashaaz <rashaabdelaziz111@gmail.com>
2024-04-09 08:03:14 -0700

    modfy

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,7 +4,7 @@ import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts listed for a given subreddit."""
+    """Prints the titles of the first 10 hot posts listed"""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
