dd5cba588d13ee713555e2a05220a1681cc6bc66
rashaaz <rashaabdelaziz111@gmail.com>
2024-04-09 08:29:44 -0700

    modfy

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,11 +9,21 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    headers = {
+        "User-Agent": "My Reddit API Client (by /u/your_username)"
+    }
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
+        elif response.status_code == 404:
+            print("Subreddit '{}' not found.".format(subreddit))
+            return 0
+        else:
+            print("Error: {}".format(response.status_code))
+            return 0
+    except Exception as e:
+        print("An error occurred: {}".format(e))
         return 0
